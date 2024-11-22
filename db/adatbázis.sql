377c6be07dd84faf88630c29b6193f4f973789ab
Bence <eidzokap@gmail.com>
2024-11-22 15:56:07 +0100

    Rename database files and update SQL schema
    Renamed "adatbázis modell.mwb" and its backup to a new directory. Enhanced SQL schema by adding new tables: `szezon` and `stab`, and temporarily disabling checks for SQL mode, unique, and foreign key constraints to ensure smoother schema setup. Also, broke down long foreign key constraint lines for readability.

--- db/adatbázis.sql
+++ db/adatbázis.sql
@@ -1,3 +1,9 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+CREATE SCHEMA IF NOT EXISTS `hazi` DEFAULT CHARACTER SET utf8 ;
+
 CREATE TABLE IF NOT EXISTS `hazi`.`felhasznalo`
 (
     `id`              INT(11)                          NOT NULL AUTO_INCREMENT,
@@ -25,11 +31,20 @@ CREATE TABLE IF NOT EXISTS `hazi`.`anime`
     `ertekeles`    FLOAT(11)                                                 DEFAULT NULL,
     PRIMARY KEY (`id`),
     CONSTRAINT `fk_anime_elozmeny`
-        FOREIGN KEY (`elozmeny_id`) REFERENCES `hazi`.`anime` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+        FOREIGN KEY (`elozmeny_id`)
+        REFERENCES `hazi`.`anime` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
     CONSTRAINT `fk_anime_folytatas`
-        FOREIGN KEY (`folytatas_id`) REFERENCES `hazi`.`anime` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+        FOREIGN KEY (`folytatas_id`)
+        REFERENCES `hazi`.`anime` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
     CONSTRAINT `fk_anime_szezon`
-        FOREIGN KEY (`szezon_id`) REFERENCES `hazi`.`szezon` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+        FOREIGN KEY (`szezon_id`)
+        REFERENCES `hazi`.`szezon` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
@@ -51,9 +66,11 @@ CREATE TABLE IF NOT EXISTS `hazi`.`anime_lista`
     `ertekeles`      INT(11)                                        NULL DEFAULT NULL,
     PRIMARY KEY (`felhasznalo_id`, `anime_id`),
     CONSTRAINT `fk_felhasznalo_has_anime_felhasznalo1`
-        FOREIGN KEY (`felhasznalo_id`) REFERENCES `hazi`.`felhasznalo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+        FOREIGN KEY (`felhasznalo_id`)
+        REFERENCES `hazi`.`felhasznalo` (`id`),
     CONSTRAINT `fk_felhasznalo_has_anime_anime1`
-        FOREIGN KEY (`anime_id`) REFERENCES `hazi`.`anime` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+        FOREIGN KEY (`anime_id`)
+        REFERENCES `hazi`.`anime` (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
@@ -63,9 +80,11 @@ CREATE TABLE IF NOT EXISTS `hazi`.`anime_has_studio`
     `studio_id` INT(11) NOT NULL,
     PRIMARY KEY (`anime_id`, `studio_id`),
     CONSTRAINT `fk_anime_has_studio_anime1`
-        FOREIGN KEY (`anime_id`) REFERENCES `hazi`.`anime` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+        FOREIGN KEY (`anime_id`)
+        REFERENCES `hazi`.`anime` (`id`),
     CONSTRAINT `fk_anime_has_studio_studio1`
-        FOREIGN KEY (`studio_id`) REFERENCES `hazi`.`studio` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+        FOREIGN KEY (`studio_id`)
+        REFERENCES `hazi`.`studio` (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
@@ -96,11 +115,14 @@ CREATE TABLE IF NOT EXISTS `hazi`.`szerep`
     `nyelv`               VARCHAR(15) NOT NULL,
     PRIMARY KEY (`anime_id`, `karakter_id`, `szinkron_szinész_id`),
     CONSTRAINT `fk_anime_has_karakter_anime1`
-        FOREIGN KEY (`anime_id`) REFERENCES `hazi`.`anime` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+        FOREIGN KEY (`anime_id`)
+        REFERENCES `hazi`.`anime` (`id`),
     CONSTRAINT `fk_anime_has_karakter_karakter1`
-        FOREIGN KEY (`karakter_id`) REFERENCES `hazi`.`karakter` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+        FOREIGN KEY (`karakter_id`)
+        REFERENCES `hazi`.`karakter` (`id`),
     CONSTRAINT `fk_anime_has_karakter_szinkron_szinész1`
-        FOREIGN KEY (`szinkron_szinész_id`) REFERENCES `hazi`.`szemely` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+        FOREIGN KEY (`szinkron_szinész_id`)
+        REFERENCES `hazi`.`szemely` (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
@@ -110,21 +132,25 @@ CREATE TABLE IF NOT EXISTS `hazi`.`kedvenc_karakter`
     `karakter_id`    INT(11) NOT NULL,
     PRIMARY KEY (`felhasznalo_id`, `karakter_id`),
     CONSTRAINT `fk_felhasznalo_has_karakter_felhasznalo1`
-        FOREIGN KEY (`felhasznalo_id`) REFERENCES `hazi`.`felhasznalo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+        FOREIGN KEY (`felhasznalo_id`)
+        REFERENCES `hazi`.`felhasznalo` (`id`),
     CONSTRAINT `fk_felhasznalo_has_karakter_karakter1`
-        FOREIGN KEY (`karakter_id`) REFERENCES `hazi`.`karakter` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+        FOREIGN KEY (`karakter_id`)
+        REFERENCES `hazi`.`karakter` (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
-CREATE TABLE IF NOT EXISTS `hazi`.`kedvenc_szinész`
+CREATE TABLE IF NOT EXISTS `hazi`.kedvenc_szinesz
 (
     `felhasznalo_id`      INT(11) NOT NULL,
     `szinkron_szinész_id` INT(11) NOT NULL,
     PRIMARY KEY (`felhasznalo_id`, `szinkron_szinész_id`),
     CONSTRAINT `fk_felhasznalo_has_szinész_felhasznalo1`
-        FOREIGN KEY (`felhasznalo_id`) REFERENCES `hazi`.`felhasznalo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+        FOREIGN KEY (`felhasznalo_id`)
+        REFERENCES `hazi`.`felhasznalo` (`id`),
     CONSTRAINT `fk_felhasznalo_has_szinész_szinész1`
-        FOREIGN KEY (`szinkron_szinész_id`) REFERENCES `hazi`.`szemely` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+        FOREIGN KEY (`szinkron_szinész_id`)
+        REFERENCES `hazi`.`szemely` (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
@@ -137,6 +163,31 @@ CREATE TABLE IF NOT EXISTS `hazi`.`episodes`
     `duration`   TIME         NOT NULL,
     `air_date`   DATE         DEFAULT NULL,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`anime_id`) REFERENCES `hazi`.`anime` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+    FOREIGN KEY (`anime_id`)
+    REFERENCES `hazi`.`anime` (`id`)
+        ON DELETE CASCADE
+        ON UPDATE NO ACTION
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `hazi`.`szezon` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `szezon` VARCHAR(11) NOT NULL,
+    PRIMARY KEY (`id`))
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `hazi`.stab (
+    anime_id INT(11) NOT NULL,
+    szemely_id INT(11) NOT NULL,
+    `pozicio` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (anime_id, szemely_id),
+    CONSTRAINT `fk_Anime_has_ember_Anime1`
+        FOREIGN KEY (anime_id)
+            REFERENCES `hazi`.`Anime` (`id`),
+    CONSTRAINT `fk_Anime_has_ember_ember1`
+        FOREIGN KEY (szemely_id)
+            REFERENCES `hazi`.`szemely` (`id`)
+    )
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8;
