ea53faa0e2d9bb51072a9b2340f919f2cf2205b0
Bence <eidzokap@gmail.com>
2024-11-29 00:12:26 +0100

    listára uj anime felvétele müködik a már rajta lévő szerkesztése hátra van

--- anime.php
+++ anime.php
@@ -2,13 +2,13 @@
 session_start();
 if(empty($_GET['id'])){
     http_response_code(400);
-    exit("400 nincs ilyen id");
+    exit("400 nincs id");
 }
 else {
     $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
     if ($id === false) {
         http_response_code(400);
-        exit("400 nincs ilyen id");
+        exit("400 nincs id");
     }
 }
 $dsn = 'mysql:host=localhost;dbname=hazi;charset=utf8';
@@ -20,6 +20,23 @@ try{
 }catch(PDOException $e){
     die($e->getMessage());
 }
+
+if(isset($_POST['felvesz'])){
+    $sql = "INSERT INTO anime_lista (felhasznalo_id, anime_id";
+        if(isset($_POST['ertesites'])) $sql.= ", ertekeles";
+        if(isset($_POST['hol_tartasz'])) $sql.=", hol_tart";
+        $sql.=", statusz) VALUES (:user_id, :anime_id";
+        if(isset($_POST['ertesites'])) $sql.= ", :ertesites";
+        if(isset($_POST['hol_tartasz'])) $sql.= ", :hol_tartasz";
+        $sql.=", NULL)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
+    $stmt->bindParam(':anime_id', $id, PDO::PARAM_INT);
+    $stmt->bindParam(':ertesites', $_POST['ertesites'], PDO::PARAM_INT);
+    $stmt->bindParam(':hol_tartasz', $_POST['hol_tartasz'], PDO::PARAM_STR);
+    $stmt->execute();
+}
+
 $sql = "SELECT * FROM anime WHERE id = :id";
 $stmt = $pdo->prepare($sql);
 $stmt->bindParam(':id', $id, PDO::PARAM_INT);
@@ -29,7 +46,6 @@ $result = $stmt->fetch(PDO::FETCH_ASSOC);
 if(!$result){
     http_response_code(404);
     exit("$id nincs ilyen id");
-
 }
 ?>
 <!DOCTYPE html>
@@ -60,17 +76,44 @@ if(!$result){
         <?php endif; ?>
     </nav>
 </header>
-
-<main>
-    <section class="anime-profile">
+<div class="row">
+    <aside class="sidebar">
         <h1><?= htmlspecialchars($result['romanji_cim']); ?></h1>
         <?php if (isset($result['angol_cim'])): ?>
-        <h2><?= htmlspecialchars($result['angol_cim']); ?></h2>
+            <h2><?= htmlspecialchars($result['angol_cim']); ?></h2>
         <?php endif; ?>
         <?php if (isset($result['poszter'], $result['romanji_cim'])): ?>
-            <img src="<?= htmlspecialchars($result['poszter'], ENT_QUOTES, 'UTF-8'); ?>"
-                 alt="<?= htmlspecialchars($result['romanji_cim'], ENT_QUOTES, 'UTF-8'); ?>">
+        <img src="<?= htmlspecialchars($result['poszter'], ENT_QUOTES, 'UTF-8'); ?>"
+             alt="<?= htmlspecialchars($result['romanji_cim'], ENT_QUOTES, 'UTF-8'); ?>">
         <?php endif; ?>
+        <?php if (isset($result['szezon_id'])): ?>
+        Szezon:
+        <a href="szezon.php?id=<?= htmlspecialchars($result['szezon_id']); ?>" >
+            <?php
+            $sqlSzezon = "SELECT ev ,szezon FROM szezon WHERE id = :id";
+            $stmtSzezon = $pdo->prepare($sqlSzezon);
+            $stmtSzezon->bindParam(':id', $result['szezon_id'], PDO::PARAM_INT);
+            $stmtSzezon->execute();
+            $szezon=$stmtSzezon->fetch(PDO::FETCH_ASSOC);
+            echo $szezon['ev'] . " " . $szezon['szezon'];
+            ?></a>
+        <?php endif; ?>
+        <?php if (isset($_SESSION['user_id'])): ?>
+        <form action="anime.php?id=<?= $_GET['id']?>" method="post">
+            <label for="hol_tartasz">Hol tartasz</label>
+            <input type="text" name="hol_tartasz" id="hol_tartasz" value="0" required>
+                   <!--value="< htmlspecialchars($_POST['hol_tartasz']); ?>"-->
+
+            <label for="ertesites">Értékelés</label>
+            <input type="number" name="ertesites" id="ertesites">
+                  <!-- value="< htmlspecialchars($_POST['ertesites']); ?>" -->
+            <input type="submit" value="Felvesz" name="felvesz">
+        </form>
+        <?php endif; ?>
+    </aside>
+<main>
+    <section class="anime-profile">
+
         <p><?= htmlspecialchars($result['leiras']); ?></p>
         <?php if (isset($result['folytatas_id'])): ?>
         <p> Folytatás:
@@ -115,6 +158,7 @@ if(!$result){
         <?php endif; ?>
     </section>
 </main>
+</div>
 </div>
 </body>
 </html>
