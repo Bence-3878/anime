10f5a61864ac5d35a36d6adab2cbeb0953a459e7
Bence <eidzokap@gmail.com>
2024-11-29 04:12:45 +0100

    fix

--- profil.php
+++ profil.php
@@ -1,9 +1,10 @@
 <?php
-
 session_start();
 
-if (!isset($_SESSION['user_id']) && !isset($_GET['id']) && !isset($_GET['name']))
+if (!isset($_SESSION['user_id']) && !isset($_GET['id'])) {
     header('Location: bejelentkezes.php');
+    exit();
+}
 
 elseif (isset($_POST['kijelentkezes']) && isset($_SESSION['user_id'])) {
     $_SESSION = array();
@@ -14,14 +15,16 @@ elseif (isset($_POST['kijelentkezes']) && isset($_SESSION['user_id'])) {
 elseif (isset($_GET['id']) && isset($_SESSION['user_id']) && $_SESSION['user_id'] == $_GET['id'])
     $user_id = $_SESSION['user_id'];
 
-elseif (isset($_GET['id']))
-    $user_id = $_GET['id'];
-
-elseif (isset($_GET['name']))
-    $user_name = $_GET['name'];
-
-elseif(isset($_SESSION['user_id']) && !isset($_GET['id']) && !isset($_GET['name']))
+elseif (isset($_GET['id'])) {
+    $user_id = $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+    if (!$user_id) {
+        http_response_code(400);
+        die("Ez nem id");
+    }
+}
+elseif(isset($_SESSION['user_id']) && !isset($_GET['id']))
     $user_id = $_SESSION['user_id'];
+
 else {
     http_response_code(400);
     die('Az id és a felhasználó név nem egy felhasználóhoz tartozik');
@@ -41,35 +44,35 @@ try {
 }
 
 $sikeres = false;
-    if(isset($_POST['user_name']) && isset($_POST['jelszo']) && isset($_SESSION['user_id'])
-        && $_SESSION['user_id'] == $user_id){
-        $sqlfelhasznalo = 'UPDATE felhasznalo SET nev = :nev WHERE id = :id';
-        $stmt = $pdo->prepare($sqlfelhasznalo);
-        $stmt->bindParam(':nev', $_POST['user_name']);
-        $stmt->bindParam(':id', $user_id);
-        $stmt->execute();
-        $sikeres = true;
-    }
+if(isset($_POST['user_name']) && isset($_POST['jelszo']) && isset($_SESSION['user_id'])
+    && $_SESSION['user_id'] == $user_id){
+    $sqlfelhasznalo = 'UPDATE felhasznalo SET nev = :nev WHERE id = :id';
+    $stmt = $pdo->prepare($sqlfelhasznalo);
+    $stmt->bindParam(':nev', $_POST['user_name']);
+    $stmt->bindParam(':id', $user_id);
+    $stmt->execute();
+    $sikeres = true;
+}
 
-    if(isset($_POST['regi_jelszo']) && isset($_POST['uj_jelszo'])
-        && isset($_POST['uj_jelszo2']) && isset($_SESSION['user_id']) && $_SESSION['user_id'] == $user_id){
-        $sqlfelhasznalo = 'SELECT jelszo FROM felhasznalo WHERE id = :id';
-        $stmt = $pdo->prepare($sqlfelhasznalo);
-        $stmt->bindParam(':id', $user_id);
-        $stmt->execute();
-        $jelszo = $stmt->fetchColumn(PDO::FETCH_ASSOC);
-        if(password_verify($_POST['regi_jelszo'], $jelszo)){
-            if($_POST['uj_jelszo'] == $_POST['uj_jelszo2']){
-                $sqlfelhasznalo = 'UPDATE felhasznalo SET jelszo = :jelszo WHERE id = :id';
-                $stmt = $pdo->prepare($sqlfelhasznalo);
-                $jelszo = password_hash($_POST['uj_jelszo'], PASSWORD_DEFAULT);
-                $stmt->bindParam(':jelszo', $jelszo);
-                $stmt->bindParam(':id', $user_id);
-                $stmt->execute();
-                $sikeres = true;
-            }
+if(isset($_POST['regi_jelszo']) && isset($_POST['uj_jelszo'])
+    && isset($_POST['uj_jelszo2']) && isset($_SESSION['user_id']) && $_SESSION['user_id'] == $user_id){
+    $sqlfelhasznalo = 'SELECT jelszo FROM felhasznalo WHERE id = :id';
+    $stmt = $pdo->prepare($sqlfelhasznalo);
+    $stmt->bindParam(':id', $user_id);
+    $stmt->execute();
+    $jelszo = $stmt->fetchColumn(PDO::FETCH_ASSOC);
+    if(password_verify($_POST['regi_jelszo'], $jelszo)){
+        if($_POST['uj_jelszo'] == $_POST['uj_jelszo2']){
+            $sqlfelhasznalo = 'UPDATE felhasznalo SET jelszo = :jelszo WHERE id = :id';
+            $stmt = $pdo->prepare($sqlfelhasznalo);
+            $jelszo = password_hash($_POST['uj_jelszo'], PASSWORD_DEFAULT);
+            $stmt->bindParam(':jelszo', $jelszo);
+            $stmt->bindParam(':id', $user_id);
+            $stmt->execute();
+            $sikeres = true;
         }
     }
+}
 
 if(isset($_POST['option_selected']) && isset($_SESSION['user_id']) && $_SESSION['jog'] == 'admin'){
     $sqlfelhasznalo = 'UPDATE felhasznalo SET jogosultsag = :jogosultsag WHERE id = :id';
@@ -91,26 +94,16 @@ if (isset($user_id)) {
         die('felhasználó nem található ilyen id-val');
     }
     $user_name = $user['nev'];
-} elseif (isset($user_name)) {
-    $sqlfelhasznalo = 'SELECT * FROM felhasznalo WHERE nev = :nev';
-    $stmt = $pdo->prepare($sqlfelhasznalo);
-    $stmt->bindParam(':nev', $user_id);
-    $stmt->execute();
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-    if(!$user){
-        http_response_code(404);
-        die('felhasználó nem található ilyen névvel');
-    }
-    $user_name = $user['id'];
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="hu">
 <head>
     <meta charset="UTF-8">
-    <title><?=$user_name?></title>
+    <title><?=htmlspecialchars($user_name)?></title>
     <link rel="stylesheet" href="style.css">
+    <!-- JQuery könyvtár betöltése -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
 <div class="container">
@@ -134,62 +127,32 @@ if (isset($user_id)) {
     <div class="row">
         <aside class="sidebar">
             <h2>Felhasználói adatok</h2>
-            <p><strong>Név:</strong> <?= $user_name ?></p>
+            <p><strong>Név:</strong> <?= htmlspecialchars($user_name) ?></p>
             <p><strong>Felhasználó ID:</strong> <?= $user_id ?></p>
             <?php if ($sikeres): ?>
-            <p class="sikeres" >Szikeren modósult</p>
+                <p class="sikeres" >Szikeren modósult</p>
             <?php endif; ?>
             <?php if (isset($_SESSION['user_id']) && $_SESSION['user_id'] == $user_id): ?>
-            <div id="felhasznalo_modositas">
-                <form action="profil.php" method="post">
-                    <h3>Felhasználó név modositás</h3>
+                <button id="open_felhasznalonev">Felhasználó módosítása</button>
 
-                    <label for="user_name">Felhasználó Név</label>
-                    <input type="text" name="user_name" id="user_name" value="<?= $user_name ?>" required>
+                <button id="open_jelszo">Jelszó módosítása</button>
 
-                    <label for="jelszo">Jelszó</label>
-                    <input type="password" name="jelszo" id="jelszo" required>
-
-                    <input type="submit" class="modosit" value="modosit">
-                </form>
-            </div>
-            <div id="jelszo_modositas">
-                <form action="profil.php" method="post">
-                    <h3>Jelszó modositás</h3>
-
-                    <label for="regi_jelszo">Régi jelszó</label>
-                    <input type="password" name="regi_jelszo" id="regi_jelszo" required>
-
-                    <label for="uj_jelszo">új jelszó</label>
-                    <input type="password" name="uj_jelszo" id="uj_jelszo" required>
-                    <?php if (isset($_POST['regi_jelszo']) && isset($_POST['uj_jelszo'])
-                        && isset($_POST['uj_jelszo2']) && isset($_SESSION['user_id'])
-                        && $_SESSION['user_id'] == $user_id && $_POST['uj_jelszo'] != $_POST['uj_jelszo2']): ?>
-                        <p class="error">A jelszavak nem egyeznek meg!</p>
-                    <?php endif; ?>
-
-                    <label for="uj_jelszo2">Új jelszó még egyszer</label>
-                    <input type="password" name="uj_jelszo2" id="uj_jelszo2" required>
-
-                    <input type="submit" class="modosit" value="modosit">
-                </form>
-            </div>
             <?php endif; ?>
             <?php if (isset($_SESSION['jog']) && $_SESSION['jog'] == 'admin'): ?>
-            <form action="profil.php?id=<?=$user_id?>" method="post">
-                <select name="option_selected" id="option_selected" required>
-                <option value="user" <?php if ($user['jogosultsag'] == 'user') echo "disabled selected"?>>User</option>
-                <option value="editor" <?php if ($user['jogosultsag'] == 'editor') echo "disabled selected"?>>Editor</option>
-                <option value="admin" <?php if ($user['jogosultsag'] == 'admin') echo "disabled selected"?>>Admin</option>
-                </select>
+                <form action="profil.php?id=<?=$user_id?>" method="post">
+                    <select name="option_selected" id="option_selected" required>
+                        <option value="user" <?php if ($user['jogosultsag'] == 'user') echo "disabled selected"?>>User</option>
+                        <option value="editor" <?php if ($user['jogosultsag'] == 'editor') echo "disabled selected"?>>Editor</option>
+                        <option value="admin" <?php if ($user['jogosultsag'] == 'admin') echo "disabled selected"?>>Admin</option>
+                    </select>
 
-                <input type="submit" class="modosit" value="modosit">
-            </form>
+                    <input type="submit" class="modosit" value="modosit">
+                </form>
             <?php endif; ?>
             <?php if (isset($_SESSION['user_id']) && $_SESSION['user_id'] == $user_id): ?>
-            <form action="profil.php" method="post">
-                <input type="submit" class="kijelenkezes" name="kijelentkezes" value="kijelentkezes">
-            </form>
+                <form action="profil.php" method="post">
+                    <input type="submit" class="kijelenkezes" name="kijelentkezes" value="kijelentkezes">
+                </form>
             <?php endif; ?>
         </aside>
         <main class="content">
@@ -198,5 +161,71 @@ if (isset($user_id)) {
         </main>
     </div>
 </div>
+
+<!-- A modális ablak -->
+<div id="felhasznalo_modositas_modal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <form action="profil.php" method="post">
+            <h3>Felhasználó név módosítás</h3>
+            <label for="user_name">Felhasználó Név</label>
+            <input type="text" name="user_name" id="user_name" value="<?= htmlspecialchars($user_name) ?>" required>
+            <label for="jelszo">Jelszó</label>
+            <input type="password" name="jelszo" id="jelszo" required>
+            <input type="submit" class="modosit" value="Módosít">
+        </form>
+    </div>
+</div>
+<div id="jelszo_modositas_modal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <form action="profil.php" method="post">
+            <h3>Jelszó módosítás</h3>
+
+            <label for="regi_jelszo">Régi jelszó</label>
+            <input type="password" name="regi_jelszo" id="regi_jelszo" required>
+
+            <label for="uj_jelszo">új jelszó</label>
+            <input type="password" name="uj_jelszo" id="uj_jelszo" required>
+            <?php if (isset($_POST['regi_jelszo']) && isset($_POST['uj_jelszo'])
+                && isset($_POST['uj_jelszo2']) && isset($_SESSION['user_id'])
+                && $_SESSION['user_id'] == $user_id && $_POST['uj_jelszo'] != $_POST['uj_jelszo2']): ?>
+                <p class="error">A jelszavak nem egyeznek meg!</p>
+            <?php endif; ?>
+
+            <label for="uj_jelszo2">Új jelszó még egyszer</label>
+            <input type="password" name="uj_jelszo2" id="uj_jelszo2" required>
+
+            <input type="submit" class="modosit" value="Módosít">
+        </form>
+    </div>
+</div>
+<script>
+    $(document).ready(function() {
+        // A modális ablak megnyitása
+        $("#open_felhasznalonev").on('click', function() {
+            $("#felhasznalo_modositas_modal").fadeIn();
+        });
+
+        $("#open_jelszo").on('click', function() {
+            $("#jelszo_modositas_modal").fadeIn();
+        });
+
+        // A modális ablak bezárása
+        $(".close").on('click', function() {
+            $(".modal").fadeOut();
+        });
+
+        // A modális ablak bezárása, ha a tartalmon kívülre kattintunk
+        $(window).on('click', function(event) {
+            if ($(event.target).is("#felhasznalo_modositas_modal")) {
+                $("#felhasznalo_modositas_modal").fadeOut();
+            }
+            if ($(event.target).is("#jelszo_modositas_modal")) {
+                $("#jelszo_modositas_modal").fadeOut();
+            }
+        });
+    });
+</script>
 </body>
 </html>
